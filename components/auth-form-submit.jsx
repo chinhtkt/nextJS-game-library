2a9bfd8642866f2a01a577470ca6b2a457f53c5f
chinhtkt <chinhtkt@lgcns.com>
2025-05-21 16:25:41 +0700

    implemented game list

--- components/auth-form-submit.jsx
+++ components/auth-form-submit.jsx
@@ -1,17 +1,16 @@
-'use client'
 import React from 'react';
 import {useFormStatus} from "react-dom";
 
 const AuthFormSubmitButton = ({mode}) => {
     const {pending} = useFormStatus()
     return (
-            <button
-                disabled={pending}
-                type="submit"
-                className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition-colors cursor-pointer"
-            >
-                {mode === 'login' ? 'Login'  : 'Create Account'}
-            </button>
+        <button
+            disabled={pending}
+            type="submit"
+            className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition-colors cursor-pointer"
+        >
+            {mode === 'login' ? 'Login' : 'Create Account'}
+        </button>
     );
 };
 
